d6c3e78fb38a7b1ad4ec32cdb010bf238f1cb143
larunrahul <rahul.arun86@gmail.com>
2017-05-23 21:53:12 +0530

    adding more tests

--- test/math/power.js
+++ test/math/power.js
@@ -5,17 +5,23 @@ describe("Math", function() {
         it("power to zero", function() {
             assert.equal(1, api.power(2, 0));
         });
-        it("power to positive number", function() {
+        it("power of +ve int to +ve int", function() {
             assert.equal(32, api.power(2, 5));
         });
-        it("power to negative number", function() {
+        it("power of +ve int to -ve int", function() {
             assert.equal(0.03125, api.power(2, -5));
         });
-        it("power of negative number", function() {
+        it("power of -ve int to +ve int odd", function() {
             assert.equal(-32, api.power(-2, 5));
         });
-        it("power of negative number with even exponent", function() {
+        it("power of -ve int to +ve int even", function() {
             assert.equal(16, api.power(-2, 4));
+        });
+        it("power of -ve int to -ve int odd", function() {
+            assert.equal(-0.03125, api.power(-2, -5));
+        });
+        it("power of -ve int to -ve int even", function() {
+            assert.equal(0.0625, api.power(-2, -4));
         });
     });
 });
