9b6a8bbd6425d15bf927d7c7fecf68a3c6ef007d
larunrahul <rahul.arun86@gmail.com>
2017-07-03 22:53:39 +0530

    Changed opration name increaseKey -> decreaseKey

--- src/datastructure/MinPriorityQueue.js
+++ src/datastructure/MinPriorityQueue.js
@@ -16,10 +16,10 @@ module.exports = (function() {
         return min;
     }
 
-    function increaseKey(node, key) {
+    function decreaseKey(node, key) {
         let heap = this.minHeap.heap;
-        if (key < heap.array[node]) {
-            throw new Error("New key is smaller than current key");
+        if (key > heap.array[node]) {
+            throw new Error("New key is greater than current key");
         }
         heap.array[node] = key;
         while (node > 0 && heap.array[this.minHeap.parent(node)] > heap.array[node]) {
@@ -34,14 +34,14 @@ module.exports = (function() {
         let heap = this.minHeap.heap;
         heap.heapSize += 1;
         heap.array[heap.heapSize] = Math.NEGATIVE_INFINITY;
-        this.increaseKey(heap.heapSize, key);
+        this.decreaseKey(heap.heapSize, key);
     }
 
     return function MinPriorityQueue(array) {
         this.minHeap = new MinHeap(array);
         this.minimum = minimum;
         this.extractMinimum = extractMinimum;
-        this.increaseKey = increaseKey;
+        this.decreaseKey = decreaseKey;
         this.insert = insert;
     }
 })();
